91531665b1cf531363179857be86c62fd430339e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-05-01 10:35:13 -0400

    Remove original data from package

--- R/utils.R
+++
@@ -1,8 +0,0 @@
-##' @importFrom downloader download
-download_maybe <- function(url, dest, refetch=FALSE, path=".") {
-  if (refetch || !file.exists(dest)) {
-    dir.create(dirname(dest), FALSE, TRUE)
-    downloader::download(url, dest)
-  }
-  invisible(dest)
-}
