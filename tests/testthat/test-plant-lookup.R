a2f36b44dd898348b59abdc4b69200e556f13507
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-05-02 14:49:54 -0400

    Update to reflect tests

--- tests/testthat/test-plant-lookup.R
+++ tests/testthat/test-plant-lookup.R
@@ -3,10 +3,11 @@ context("plant_lookup")
 ## Need to do something nice here with testing?
 test_that("table sane", {
   lookup <- plant_lookup()
-  expect_that(ncol(lookup), equals(4L))
+  expect_that(ncol(lookup), equals(5L))
   expect_that(lookup, is_a("data.frame"))
   expect_that(names(lookup),
-              equals(c("genus", "family", "order", "group")))
+              equals(c("number.of.species",
+                       "genus", "family", "order", "group")))
   expect_that(any(is.na(lookup)), is_false())
   expect_that(any(lookup == ""), is_false())
   expect_that(any(duplicated(lookup$genus)), is_false())
