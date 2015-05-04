1be559a935062d09e2bee0001b3d24db4e6f690a
wcornwell <wcornwell@gmail.com>
2015-05-04 16:55:40 -0400

    adding more documentation

--- R/higher_order.R
+++ R/higher_order.R
@@ -1,10 +1,32 @@
-##' Augment a lookup table with plant higher order data, from
+##' Augment the genus-family-order lookup table with plant higher order data, from
 ##' http://datadryad.org/resource/doi:10.5061/dryad.63q27
+##'
+##'
+##' Data for the higher-level taxonomy lookup (http://datadryad.org/resource/doi:10.5061/dryad.63q27.2/1.1) compiled by Dave Tank and colleagues
+##'
+##' The format of the lookup is that higher nodes are columns within the data.frame.
+##' If a genus is a descendent of a particular node, the row for that genus repeats
+##' the node name.  If a genus is not a descendent of that particular node, the cell is left blank.
+##'
 ##' @title Add higher order taxonomy
 ##' @param lookup A lookup table (by default \code{plant_lookup()})
 ##' @param order_column The column within \code{lookup} that
 ##' corresponds to taxonomic order.
 ##' @export
+##' @examples
+##'
+##' ##' # see the format of the resource
+##' head(add_higher_order())
+##' #
+##' # load the data.frame into memory
+##' ho<-add_higher_order()
+##' #
+##' # return Rosid orders
+##' unique(ho$order[ho$Rosidae=="Rosidae"])
+##' #
+##' # find the number of Conifer species in the world
+##' sum(ho$number.of.species[ho$Coniferae=="Coniferae"])
+##'
 add_higher_order <- function(lookup=plant_lookup(), order_column="order") {
   hot <- higher_order_taxonomy[lookup[[order_column]], ]
   rownames(hot) <- NULL
