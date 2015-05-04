674aa3524f9cae4f171580aada1e2e63e6038a7f
wcornwell <wcornwell@gmail.com>
2015-05-04 13:43:16 -0400

    adding some documentation and small changes to version handling

--- R/plant_lookup.R
+++ R/plant_lookup.R
@@ -1,11 +1,37 @@
-##' Lookup table relating plant genera, families and orders.  Data
+##' Lookup table relating plant genera, families and orders along with number of species in each genus.  Data
 ##' persists across package installations.
+##'
+##' The data for this lookup primarily comes from three sources:
+##'
+##' 1. The Plant List v1.1. (http://www.theplantlist.org/) for accepted genera to families and species richness within each genera.  Note that we do not consider hybrids (e.g. Genus x species) as species for this count while the plant list summary statistics do, so the counts will not line up exactly.
+##'
+##' 2. APWeb (http://www.mobot.org/MOBOT/research/APweb/) for family level synonymies and family to order
+##'
+##' 3. A higher-level taxonomy lookup (http://datadryad.org/resource/doi:10.5061/dryad.63q27.2/1.1) compiled by Dave Tank and colleagues
+##'
+##'To complete the family to order data (beyond the taxonomic scope of APWeb) we add a few additional family to order mappings for non-seed plants (mostly ferns).  We also correct some spelling errors, special character issues, and other errors from The Plant List.
 ##'
 ##' @title Plant lookup table
 ##' @param version Version number.  The default will load the most
 ##' recent version on your computer or the most recent version known
 ##' to the package if you have never downloaded the data before.
 ##' @export
+##' @examples
+##' #
+##' # see the format of the resource
+##' head(plant_lookup())
+##' #
+##' # load the data.frame into memory
+##' pl<-plant_lookup()
+##' #
+##' # return family, order, and number of species for the genus Eucalyptus
+##' pl$family[pl$genus=="Eucalyptus"]
+##' pl$order[pl$genus=="Eucalyptus"]
+##' pl$number.of.species[pl$genus=="Eucalyptus"]
+##' #
+##' # find the number of species in Asteraceae
+##' sum(pl$number.of.species[pl$family=="Asteraceae"])
+##'
 plant_lookup <- function(version=plant_lookup_version_current()) {
   if (is.null(version)) {
     version <- plant_lookup_version_current(FALSE)
@@ -27,8 +53,7 @@ plant_lookup_versions <- function(all=FALSE) {
   if (all) {
     c("0.1.0", "0.1.1","0.1.2","0.1.3","0.1.4")
   } else {
-    re <- "^plant_lookup_(.+).csv$"
-    sub(re, "\\1", dir(plant_lookup_path(), re))
+    plant_lookup_storr()$list()
   }
 }
 
@@ -37,6 +62,7 @@ plant_lookup_versions <- function(all=FALSE) {
 plant_lookup_version_current <- function(all=FALSE) {
   if (all) {
     ## TODO: This *should* ping /latest I think.
+    ## Manually add data if data and package versions are out of line
     last(plant_lookup_versions(TRUE))
   } else {
     v <- plant_lookup_storr()$list()
@@ -88,5 +114,6 @@ plant_lookup_fetch <- function(version) {
       stop("Download failed: ", first)
     }
   }
+  #TODO: add dataset version as attribute
   read.csv(dest, stringsAsFactors=FALSE)
 }
