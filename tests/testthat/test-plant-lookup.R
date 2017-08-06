fd4392faff8e6bcb71c42f7a4bb27e480c2258eb
wcornwell <wcornwell@gmail.com>
2017-08-06 20:28:31 +1000

    more tests

--- tests/testthat/test-plant-lookup.R
+++ tests/testthat/test-plant-lookup.R
@@ -19,4 +19,7 @@ test_that("table sane", {
   expect_that(any(lookup == ""), is_false())
   expect_that(any(duplicated(lookup$genus)), is_false())
   expect_is(plant_lookup_version_current(),"character")
+  only_corn<-subset(lookup,lookup$genus=="Zea")
+  expect_true(only_corn$family[1]=="Poaceae")
+  expect_true(only_corn$group[1]=="Angiosperms")
 })
